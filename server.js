2c5a0d66be88a70e057d9ca9565be9f7645ead59
Katepiss Sriburapa <artcoding2544@gmail.com>
2022-04-24 13:52:54 +0700

    done auth structure

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
-console.log('server running');
+import express from 'express';
+const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
+
+// db and authenticateUser
+import connectDB from './db/connect.js';
+
+// routers
+import authRouter from './routes/authRoutes.js';
+
+//--------------------  middleware ------------------
+import errorHandlerMiddleware from './middleware/error-handler.js';
+import notFoundMiddleware from './middleware/not-found.js';
+
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Welcome');
+});
+
+app.use('/api/v1/auth', authRouter);
+// looking for request that not math current route
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port} ...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
