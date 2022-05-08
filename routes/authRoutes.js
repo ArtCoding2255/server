deb7c759cde2116e990f9860b5739e2141bcffff
Katepiss Sriburapa <artcoding2544@gmail.com>
2022-05-08 10:10:16 +0700

    login logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,20 @@
 import express from 'express';
 const router = express.Router();
+/*import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from '../controllers/userController.js';*/
 
-import { register, login, updateUser } from '../controllers/authController.js';
+import { register, login } from '../controllers/authController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/register').post(register);
 router.route('/login').post(login);
-router.route('/updateUser').patch(updateUser);
 
 export default router;
